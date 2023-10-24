ce764f590f0f0e7afa1a860d40cf089188dbfa0b
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2023-10-24 15:25:07 +0800

    Refactor and edit doc
    Refactor and edit doc

--- test/unit_test/test_bot/test_bot.py
+++ test/unit_test/test_bot/test_bot.py
@@ -7,14 +7,21 @@ from re_edge_gpt import ConversationStyle
 
 
 async def test_ask() -> None:
-    bot = await Chatbot.create(cookies=json.loads(getenv("EDGE_COOKIES")))
-    response = await bot.ask(
-        prompt="find me some information about the new ai released by meta.",
-        conversation_style=ConversationStyle.balanced
-    )
-    await bot.close()
-    print(json.dumps(response, indent=2))
-    assert response
+    bot = None
+    try:
+        bot = await Chatbot.create(cookies=json.loads(getenv("EDGE_COOKIES")))
+        response = await bot.ask(
+            prompt="find me some information about the new ai released by meta.",
+            conversation_style=ConversationStyle.balanced
+        )
+        await bot.close()
+        print(json.dumps(response, indent=2))
+        assert response
+    except Exception as error:
+        raise error
+    finally:
+        if bot is not None:
+            await bot.close()
 
 
 if __name__ == "__main__":
