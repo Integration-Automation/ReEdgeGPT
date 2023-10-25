35823b09334c1206a2fc08d17999419a16a7b922
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2023-10-25 09:53:27 +0800

    Update version and test

--- re_edge_gpt/__init__.py
+++ re_edge_gpt/__init__.py
@@ -1,7 +1,7 @@
-from re_edge_gpt.re_edge_gpt import Chatbot
-from re_edge_gpt.re_edge_gpt import ConversationStyle
 from re_edge_gpt.image_genearation import ImageGen
 from re_edge_gpt.image_genearation import ImageGenAsync
+from re_edge_gpt.re_edge_gpt import Chatbot
+from re_edge_gpt.re_edge_gpt import ConversationStyle
 
 __all__ = [
     "Chatbot", "ConversationStyle", "ImageGen", "ImageGenAsync"
