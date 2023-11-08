1d99aff4da2be4038df15e7d11538bfe1050d306
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2023-11-08 17:24:37 +0800

    Make dockerfile example return json
    Make dockerfile example return json

--- Dockerfiles/Flask/re_edge_gpt_blueprint.py
+++ Dockerfiles/Flask/re_edge_gpt_blueprint.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 from re_edge_gpt import Chatbot, ConversationStyle
 
@@ -11,16 +11,23 @@ re_edge_gpt_blueprint_instance = Blueprint("re_edge_gpt_blueprint", __name__, ur
 @re_edge_gpt_blueprint_instance.route("/chat", methods=["POST"])
 async def chat():
     prompt = request.get_json()["prompt"]
+    conversation_style = request.get_json()["conversation_style"]
+    conversation_style = {
+        "creative": ConversationStyle.creative,
+        "balanced": ConversationStyle.balanced,
+        "precise": ConversationStyle.precise,
+    }.get(conversation_style, ConversationStyle.balanced)
+    simplify_response = request.get_json()["simplify_response"]
     bot = None
     try:
         cookies = json.loads(open(str(Path(str(Path.cwd()) + "/bing_cookies.json")), encoding="utf-8").read())
         bot = await Chatbot.create(cookies=cookies)
         response = await bot.ask(
             prompt=prompt,
-            conversation_style=ConversationStyle.balanced,
-            simplify_response=True
+            conversation_style=conversation_style,
+            simplify_response=simplify_response
         )
-        return json.dumps(response, indent=2, ensure_ascii=False)
+        return jsonify(response)
     except Exception as error:
         raise error
     finally:
