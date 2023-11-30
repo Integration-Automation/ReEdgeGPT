1692b5644bdd6dfed3a9deff40eef4793a707291
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2023-11-30 18:00:59 +0800

    Add get and set conversation
    Add get and set conversation

--- test/unit_test/manual_test/test_conversation.py
+++ test/unit_test/manual_test/test_conversation.py
@@ -5,9 +5,10 @@ from pathlib import Path
 from re_edge_gpt import Chatbot
 from re_edge_gpt import ConversationStyle
 
-
 # If you are using jupyter pls install this package
 # from nest_asyncio import apply
+
+conversation_dict = {}
 
 
 async def test_ask() -> None:
@@ -17,15 +18,35 @@ async def test_ask() -> None:
             str(Path(str(Path.cwd()) + "/bing_cookies.json")), encoding="utf-8").read())
         bot = await Chatbot.create(cookies=cookies)
         response = await bot.ask(
-            prompt="How to extract gold",
+            prompt="Translate next word what I say to english",
             conversation_style=ConversationStyle.balanced,
             simplify_response=True
         )
         # If you are using non ascii char you need set ensure_ascii=False
         print(json.dumps(response, indent=2, ensure_ascii=False))
-        # Raw response
-        # print(response)
-        assert response
+        print(await bot.chat_hub.get_conversation())
+        conversation_dict.update(await bot.chat_hub.get_conversation())
+    except Exception as error:
+        raise error
+    finally:
+        if bot is not None:
+            await bot.close()
+
+
+async def test_ask_conversation() -> None:
+    bot = None
+    try:
+        cookies: list[dict] = json.loads(open(
+            str(Path(str(Path.cwd()) + "/bing_cookies.json")), encoding="utf-8").read())
+        bot = await Chatbot.create(cookies=cookies)
+        await bot.chat_hub.set_conversation(conversation_dict=conversation_dict)
+        response = await bot.ask(
+            prompt="піца",
+            conversation_style=ConversationStyle.balanced,
+            simplify_response=True
+        )
+        # If you are using non ascii char you need set ensure_ascii=False
+        print(json.dumps(response, indent=2, ensure_ascii=False))
     except Exception as error:
         raise error
     finally:
@@ -41,3 +62,4 @@ if __name__ == "__main__":
     except RuntimeError:
         loop = asyncio.get_event_loop()
     loop.run_until_complete(test_ask())
+    loop.run_until_complete(test_ask_conversation())
