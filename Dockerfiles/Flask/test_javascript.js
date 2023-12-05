5b4309ebffdd10e29c27f2313fba6cc177f9462e
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2023-12-05 17:33:44 +0800

    Update docker and API
    Update docker and API

--- Dockerfiles/Flask/test_javascript.js
+++ Dockerfiles/Flask/test_javascript.js
@@ -1,15 +1,34 @@
-let httpRequest = new XMLHttpRequest();
-httpRequest.open("POST", "/ReEdgeGPT/chat", true)
-httpRequest.setRequestHeader("content-type", "application/json")
-httpRequest.send(JSON.stringify({
-    "prompt": "Deer stew",
-    "conversation_style": "balanced",
-    "simplify_response": true
-}))
-httpRequest.onreadystatechange = function () {
-    if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
-        console.log(httpRequest.response)
-    } else if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status !== 200) {
-        console.log(httpRequest.response)
+function callChatbot() {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("POST", "/ReEdgeGPT/chat", true)
+    httpRequest.setRequestHeader("content-type", "application/json")
+    httpRequest.send(JSON.stringify({
+        "prompt": "Deer stew",
+        "conversation_style": "balanced",
+        "simplify_response": true,
+        "attachment": null
+    }))
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+            console.log(httpRequest.response)
+        } else if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status !== 200) {
+            console.log(httpRequest.response)
+        }
+    }
+}
+
+function callImagebot() {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.open("POST", "/ReEdgeGPT/image", true)
+    httpRequest.setRequestHeader("content-type", "application/json")
+    httpRequest.send(JSON.stringify({
+        "prompt": "La Tour Eiffel",
+    }))
+    httpRequest.onreadystatechange = function () {
+        if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status === 200) {
+            console.log(httpRequest.response)
+        } else if (httpRequest.readyState === XMLHttpRequest.DONE && httpRequest.status !== 200) {
+            console.log(httpRequest.response)
+        }
     }
 }
