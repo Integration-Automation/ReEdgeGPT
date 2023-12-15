4f26b075ad1083012e10fbd1bf2fea456f6042ef
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2023-12-15 10:43:01 +0800

    Change some mode
    Change some mode

--- re_edge_gpt/constants.py
+++ re_edge_gpt/constants.py
@@ -59,7 +59,7 @@ HEADERS_INIT_CONVER = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
                   "Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
-    "x-edge-shopping-flag": "1",
+    "x-edge-shopping-flag": "0",
     "x-forwarded-for": FORWARDED_IP,
 }
 
--- test/unit_test/manual_test/test_bot_manual.py
+++ test/unit_test/manual_test/test_bot_manual.py
@@ -17,7 +17,7 @@ async def test_ask() -> None:
             str(Path(str(Path.cwd()) + "/bing_cookies.json")), encoding="utf-8").read())
         bot = await Chatbot.create(cookies=cookies)
         response = await bot.ask(
-            prompt="How do I say Miso soup in japanese",
+            prompt="Are you using copilot",
             conversation_style=ConversationStyle.balanced,
             simplify_response=True
         )
