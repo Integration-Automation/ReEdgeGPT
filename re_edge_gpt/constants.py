1bf8724923d723812fefd72d9b7122bd01febc3c
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2024-01-09 16:52:21 +0800

    Update constants and dockfiles
    Update constants and dockfiles

--- re_edge_gpt/constants.py
+++ re_edge_gpt/constants.py
@@ -13,15 +13,15 @@ HEADERS = {
     "accept-language": "en,zh-TW;q=0.9,zh;q=0.8,en-GB;q=0.7,en-US;q=0.6",
     "accept-encoding": "gzip, deflate, br, zsdch",
     "content-type": "application/json",
-    "sec-ch-ua": '"Microsoft Edge";v="119", '
-                 '"Chromium";v="119", '
-                 '"Not?A_Brand";v="24"',
+    "sec-ch-ua": '"Microsoft Edge";v="120", '
+                 '"Chromium";v="120", '
+                 '"Not?A_Brand";v="8"',
     "sec-ch-ua-arch": '"x86"',
     "sec-ch-ua-bitness": '"64"',
-    "sec-ch-ua-full-version": '"119.0.2151.44"',
-    "sec-ch-ua-full-version-list": '"Microsoft Edge";v="119.0.2151.44", '
-                                   '"Chromium";v="119.0.6045.105", '
-                                   '"Not?A_Brand";v="24.0.0.0"',
+    "sec-ch-ua-full-version": '"120"',
+    "sec-ch-ua-full-version-list": '"Microsoft Edge";v="120", '
+                                   '"Chromium";v="120", '
+                                   '"Not?A_Brand";v="8"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-model": "",
     "sec-ch-ua-platform": '"Windows"',
@@ -29,28 +29,26 @@ HEADERS = {
     "sec-fetch-dest": "empty",
     "sec-fetch-mode": "cors",
     "sec-fetch-site": "same-origin",
-    "sec-ms-gec-version": "1-119.0.2151.44",
+    "sec-ms-gec-version": "1-120",
     "x-ms-client-request-id": str(uuid.uuid4()),
-    "x-ms-useragent": "azsdk-js-api-client-factory/1.0.0-beta.1 core-rest-pipeline/1.12.0 OS/Windows",
-    "Referer": "https://www.bing.com/copilot",
+    "Referer": "https://copilot.microsoft.com/",
     "Referrer-Policy": "origin-when-cross-origin",
     "x-forwarded-for": FORWARDED_IP,
 }
 
 HEADERS_INIT_CONVER = {
-    "authority": "www.bing.com",
     "accept": "application/json",
     "accept-language": "en,zh-TW;q=0.9,zh;q=0.8,en-GB;q=0.7,en-US;q=0.6",
     "cache-control": "max-age=0",
-    "sec-ch-ua": '"Microsoft Edge";v="119", '
-                 '"Chromium";v="119", '
-                 '"Not?A_Brand";v="24"',
+    "sec-ch-ua": '"Microsoft Edge";v="120", '
+                 '"Chromium";v="120", '
+                 '"Not?A_Brand";v="8"',
     "sec-ch-ua-arch": '"x86"',
     "sec-ch-ua-bitness": '"64"',
-    "sec-ch-ua-full-version": '"119.0.2151.44"',
-    "sec-ch-ua-full-version-list": '"Microsoft Edge";v="119.0.2151.44", '
-                                   '"Chromium";v="119.0.6045.105", '
-                                   '"Not?A_Brand";v="24.0.0.0"',
+    "sec-ch-ua-full-version": '"120"',
+    "sec-ch-ua-full-version-list": '"Microsoft Edge";v="120", "'
+                                   'Chromium";v="120",'
+                                   ' "Not?A_Brand";v="8"',
     "sec-ch-ua-mobile": "?0",
     "sec-ch-ua-model": '""',
     "sec-ch-ua-platform": '"Windows"',
@@ -58,9 +56,11 @@ HEADERS_INIT_CONVER = {
     "upgrade-insecure-requests": "1",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                   "AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0",
+                  "Chrome/120.0.0.0 "
+                  "Safari/537.36 "
+                  "Edg/120.0.2210.91",
     "x-edge-shopping-flag": "0",
     "x-forwarded-for": FORWARDED_IP,
 }
 
-BUNDLE_VERSION = "1.1381.8"
+BUNDLE_VERSION = "1.1381.12"
