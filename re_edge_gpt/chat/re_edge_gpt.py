e206f4b658025f22f06140587e467168df358eff
JE-Chen <33644111+JE-Chen@users.noreply.github.com>
2024-02-18 21:02:14 +0800

    Fix iter none error
    Fix iter none error

--- re_edge_gpt/chat/re_edge_gpt.py
+++ re_edge_gpt/chat/re_edge_gpt.py
@@ -69,8 +69,9 @@ class Chatbot:
         self.proxy = proxy
         self.mode = mode
         conversation = await Conversation.create(self.proxy, cookies=cookies, mode=mode)
-        new_plugin_list = [PLUGINS.get(plugin, plugin) for plugin in plugin_ids]
-        await self.add_plugins(cookies, mode, conversation.struct["conversationId"], new_plugin_list)
+        if plugin_ids is not None:
+            new_plugin_list = [PLUGINS.get(plugin, plugin) for plugin in plugin_ids]
+            await self.add_plugins(cookies, mode, conversation.struct["conversationId"], new_plugin_list)
         self.chat_hub = ChatHub(
             conversation,
             proxy=self.proxy,
